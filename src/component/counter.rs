2cc851ae962a932243810a8ffd34b411f1024e23
Ken 🏈 <me163@mac.com>
2024-02-13 14:51:58 -0800

    counter component

---
+++ src/component/counter.rs
@@ -0,0 +1,17 @@
+use dioxus::{html::input_data::keyboard_types::Key, prelude::*};
+
+#[component]
+pub fn Counter(
+    cx: Scope,
+    count: u32,
+) -> Element {
+    render! {
+        div {
+            class: "pr-2 flex items-center",
+            div {
+                class: "rounded-full bg-gray-700 text-xs min-w-[20px] h-[20px] flex items-center justify-center",
+                "{count}"
+            }
+        }
+    }
+}
